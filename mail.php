09cbdb5e47865c285a5fe07815b7c3f869347945
Khudoley <63798241+Khudoley@users.noreply.github.com>
2023-03-26 20:15:47 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -15,18 +15,18 @@ if (!empty($_POST)) {
     $mail = new PHPMailer(true);                               // Passing `true` enables exceptions
     $mail->CharSet = 'UTF-8';
 //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output 
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'mail.hosting.reg.ru';  // Specify main and backup SMTP servers
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'info@texnoaudit.ru';                 // SMTP username
-    $mail->Password = '1Z2s6B3m';                           // SMTP password
+	 $mail->Username   = 'user@example.com';                     //SMTP username
+    $mail->Password   = 'secret';                               //SMTP password
     $mail->SMTPSecure = 'ssl';                             // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                    // TCP port to connect to
 
 //Recipients
-    $mail->setFrom('info@texnoaudit.ru', 'info@texnoaudit.ru');
-    $mail->addAddress('mail@denfrolov.ru', 'mail@denfrolov.ru');     // Add a recipient
+	 $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('clutkaboltwm@gmail.com', 'clutkaboltwm@gmail.com');     // Add a recipient
 
 //Attachments
 //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
