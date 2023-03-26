3fdca3fe8d202bd378dcfac1384d791c6d666ef6
Khudoley <63798241+Khudoley@users.noreply.github.com>
2023-03-26 21:14:24 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,48 +1,41 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-if (!empty($_POST)) {
-
-//Load Composer's autoloader
-    require $_SERVER["DOCUMENT_ROOT"] . '/phpmailer/src/Exception.php';
-    require $_SERVER["DOCUMENT_ROOT"] . '/phpmailer/src/PHPMailer.php';
-    require $_SERVER["DOCUMENT_ROOT"] . '/phpmailer/src/SMTP.php';
 
 
-    $mail = new PHPMailer(true);                               // Passing `true` enables exceptions
-    $mail->CharSet = 'UTF-8';
-//Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output 
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-	 $mail->Username   = 'user@example.com';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
-    $mail->SMTPSecure = 'ssl';                             // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465;                                    // TCP port to connect to
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
-//Recipients
-	 $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('clutkaboltwm@gmail.com', 'clutkaboltwm@gmail.com');     // Add a recipient
+if(isset($_POST["send"])) {
+	$mail = new PHPMailer(true);
 
-//Attachments
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+	$mail->isSMTP();       
+	$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'user@example.com';                     //SMTP username
+	$mail->Password   = 'secret';                               //SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+	$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+	
+	$mail->setFrom('clutkabolwm@gmail.com', 'Александр');
+   $mail->addAddress($_POST["email"]); 
 
-//Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = $_POST['subject'];
-    $message = '';
-    foreach ($_POST as $key => $item) {
-        if ($key != 'subject') {
-            $message .= $key . ' - ' . $item . '<br>';
-        }
-    }
-    $mail->Body = $message;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	$mail->isHTML(true);     
+	
+	$mail->Subject = 'Here is the subject';
+   $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+   $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
+   $mail->send();
+	echo 
+	"
+	<script>
+		alert('Сообщение отправлено')
+	</script>
+	
+	"
+	;
 }
