c1ce5679143184e6af055abe7a4229032735a679
Khudoley <63798241+Khudoley@users.noreply.github.com>
2023-03-26 19:40:15 +0300

    new site

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+	const da = new DynamicAdapt("max");
+	da.init();
+	Fancybox.bind("[data-fancybox]");
+	AOS.init();
+	$('input[type="tel"]').inputmask({ "mask": "+7(999) 999-9999" });
+
+	$(".detail-text ol").each(function (index, value) {
+		console.log(value);
+
+		let items = $(value).find('> li')
+		items.each(function (index, value) {
+			$(value).attr('data-count', index + 1)
+		});
+	});
+	$("form").submit(function (event) {
+		let form = $(this);
+		event.preventDefault();
+		form.find('button').addClass('sending');
+		$.ajax({
+			url: "/mail.php",
+			data: form.serialize(),
+			type: "POST",
+			//dataType: "json"
+		}).done(function (data) {
+			form.find('button').removeClass('sending');
+			// $.fancybox.close();
+			form[0].reset();
+			// $.fancybox.open('<div class="form-success">Сообщение отправлено!</div>');
+		});
+	});
+})
+
+
+$(document).on('click', '.js-menu__btn', function () {
+	if (!$(this).hasClass('open')) {
+		$(this).addClass('open');
+		$('.js-menu').addClass('open');
+		$('body').addClass('lock');
+	} else {
+		$(this).removeClass('open');
+		$('.js-menu').removeClass('open');
+		$('body').removeClass('lock');
+	}
+});
